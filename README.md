6a02b905e9ae64eb17b56aa59b50dca6b271e40d
Anton Ivanov <anton@imago.ai>
2017-10-25 17:03:47 +0800

    complete implementation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# getusermedia-async
-A promise-based, awaitable, browser-independent getUserMedia function to get user's audio or video.
+Asynchronous, browser-independent wrapper for the getUserMedia function.
+===================
+
+### Short sample ###
+
+```js
+var getUserMedia = require('getusermedia-async').getUserMedia;
+var audioStreamPromise = getUserMedia({ video: false, audio: true});
+// OR:
+var audioStream = await getUserMedia({ video: false, audio: true});
+```
+
+### What's it for? ###
+
+This is a promise-based, awaitable, browser-independent getUserMedia function to get user's audio or video.
+
+It is based on the `getusermedia` package by Henrik Joreteg which actually does most of the job.
+
+### Methods ###
+
+* `getUserMedia(options)`: returns a stream, or throws a `NavigatorUserMediaError`. `options` include two boolean properties: `audio` (default: `true`) and `video` (default: `true`).
+* `getUserAudio()`: shortcut to `getUserMedia({ audio: true, video: false })`.
+
+**Example:**
+
+```js
+var getUserAudio = require('getusermedia-async').getUserAudio;
+var audioStreamPromise = getUserAudio();
+```
+
+### Comments and suggestions ###
+
+If you have any comments, contact me here: https://github.com/catcher-in-the-try/
--- index.js
+++ index.js
@@ -4,12 +4,39 @@
  * @author Anton Ivanov <anton@ivanov.hk>
  */
 
+var getUserMediaSync = require('getusermedia');
+
 /**
- * A promise-based, awaitable, browser-independent getUserMedia
- * function to get user's audio or video.
+* Returns the stream of the user's audio or video, or throws
+* a `NavigatorUserMediaError`.
+* @param {object} [options] - The optional settings.
+* @param {boolean} options.video - Whether to request the video. Default: true.
+* @param {boolean} options.video - Whether to request the audio. Default: true.
+* @returns {Promise<MediaStream>}
+*/
+function getUserMediaAsync(options) {
+  return new Promise(function (resolve, reject) {
+    if (!options) options = { video: true, audio: true };
+
+    getUserMediaSync(options, function (err, stream) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(err);
+      }
+    });
+  });
+}
+
+/**
+ * Returns the user's audio stream, or throws a `NavigatorUserMediaError`.
+ * @returns {Promise<MediaStream>}
  */
-var GetUserMediaAsync = {
+function getUserAudioAsync() {
+  return getUserMediaAsync({ audio: true, video: false });
+}
 
+module.exports = {
+  getUserMedia: getUserMediaAsync,
+  getUserAudio: getUserAudioAsync,
 };
-
-module.exports = GetUserMediaAsync;
