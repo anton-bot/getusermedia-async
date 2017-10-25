2f219c38f3c67782e79bf795b0b18dae2c8bef28
Anton Ivanov <anton@imago.ai>
2017-10-25 17:03:36 +0800

    run npm init, create scaffolding

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview A promise-based, awaitable, browser-independent getUserMedia
+ * function to get user's audio or video.
+ * @author Anton Ivanov <anton@ivanov.hk>
+ */
+
+/**
+ * A promise-based, awaitable, browser-independent getUserMedia
+ * function to get user's audio or video.
+ */
+var GetUserMediaAsync = {
+
+};
+
+module.exports = GetUserMediaAsync;
